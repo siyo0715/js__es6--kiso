a6e25bf1064d09d3f1badf94c31e29a45207f3b9
向山　翔 <hms0175km@gmail.com>
2022-01-03 13:35:52 +0900

    [Add]arrow

--- arrow.js
+++ arrow.js
@@ -14,4 +14,12 @@ console.log(func1('func1です'))
 
 // 短縮
 const func2 = (str) => str;
-console.log(func2('func2です'))
+console.log(func2('func2です'));
+
+
+// 計算
+const func3 = (num1, num2) => {
+    return num1 + num2;
+};
+
+console.log(func3(10, 20))
