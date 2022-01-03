ac71e9064954ce593d38dd184f08980369bf4cc1
向山　翔 <hms0175km@gmail.com>
2022-01-03 13:32:04 +0900

    [Add]template

---
+++ template.js
@@ -0,0 +1,14 @@
+const name = "しよ";
+const age = 24;
+// 私は翔です。24歳です。
+
+const message1 = "私は" + name + "です。" + age + "歳です。";
+
+console.log(message1)
+
+
+
+// テンプレート文字列
+const message2 = `私は${name}です。${age}歳です。`;
+
+console.log(message2)
